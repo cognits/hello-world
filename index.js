2f81b26bf4738c962f97f42a963a9e2796116a39
César Reyes <cesar.reyes@cognits.co>
2024-04-29 17:03:24 -0600

    dumy 3

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const app = express();
 const port = process.env.APP_PORT || 3000;
 
 app.get('/', (req, res) => {
-    console.log('200 GET /');
+    console.log('GET /');
     res.send('Hello World!')
 });
 
 app.get('/healthcheck', (req, res) => {
-    console.log('200 GET /healthcheck');
+    console.log('GET /healthcheck');
     res.sendStatus(200);
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    console.log(`404 ${req.method} ${req.url}`);
+    console.log(`${req.method} ${req.url}`);
     res.send('Not found');
 });
 
