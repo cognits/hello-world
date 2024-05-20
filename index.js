1679195792d2b99d68359c80f43faab55d303642
César Reyes <cesar.reyes@cognits.co>
2024-05-20 17:12:00 -0600

    dummy comments

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ const express = require('express');
 const app  = express();
 const port = process.env.APP_PORT || 3000;
 
-// root path
+// / path
 app.get('/', (req, res) => {
     console.log('GET /');
     res.send('Hello World!')
 });
 
 
-// healthcheck path
+// /healthcheck path
 app.get('/healthcheck', (req, res) => {
     console.log('GET /healthcheck');
     res.sendStatus(200);
 });
 
 
-// catch 404 and forward to error handler
+// 404 middleware
 app.use(function(req, res, next) {
     console.log(`${req.method} ${req.url}`);
     res.send('Not found');
 });
 
-
+// start server
 app.listen(port, () => {
   console.log(`HELLO WORLD: started listening on port ${port}`);
 });
